c3fe07babd5e82ef96b0d80c7015d155bfb98171
Никита Целков <nikitacelkov@gmail.com>
2020-02-23 14:35:56 +0700

    Добавьте файлы проекта.

---
+++ eyeless/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace eyeless.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+
+
+
+        public ICommand GoToGamePage
+        {
+            get 
+            {
+                return new RelayCommand(() =>
+                {
+
+                });
+            }
+        }
+
+    }
+}
